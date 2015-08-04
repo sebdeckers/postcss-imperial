9d2d56ce157b82d153b8ed10308737694993b972
Sebastiaan Deckers <seb@ninja.sg>
2015-08-04 21:07:26 +0800

    Double whitespace indentation

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,21 @@ var gulp = require('gulp');
 var files = ['index.js', 'test/*.js', 'gulpfile.js'];
 
 gulp.task('lint', function (done) {
-    var eslint = require('gulp-eslint');
-    return gulp.src(files)
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError()).on('error', done);
+  var eslint = require('gulp-eslint');
+  return gulp.src(files)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError()).on('error', done);
 });
 
 gulp.task('test', function (done) {
-    var mocha = require('gulp-mocha');
-    return gulp.src('test/*.js', { read: false })
-        .pipe(mocha()).on('error', done);
+  var mocha = require('gulp-mocha');
+  return gulp.src('test/*.js', { read: false })
+    .pipe(mocha()).on('error', done);
 });
 
 gulp.task('default', ['lint', 'test']);
 
 gulp.task('watch', function() {
-    gulp.watch(files, ['lint', 'test']);
+  gulp.watch(files, ['lint', 'test']);
 });

d46c811c31962a8e94bd56e65d1b9f3ac991db8d
Sebastiaan Deckers <seb@ninja.sg>
2015-08-04 21:10:15 +0800

    Unit conversion POC

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+require('array.prototype.find');
+require('array-includes').shim();
+
 var postcss = require('postcss');
+var converters = require('./lib/converters');
+
+function translate (value, amount, unit) {
+  var convert = converters.find(function (converter) {
+    return converter.unit.includes(unit.toLowerCase());
+  }, unit);
+  return convert.css(parseFloat(amount));
+}
 
 module.exports = postcss.plugin('postcss-imperial', function (opts) {
-    opts = opts || {};
-
-    // Work with options here
-
-    return function (css) {
-
-        // Transform CSS AST here
-
-    };
+  opts = opts || {};
+  return function (css) {
+    converters.forEach(function (converter) {
+      converter.unit.forEach(function (unit) {
+        var pattern = new RegExp('^(\\d+\\.?\\d*)(' + unit + ')$', 'i');
+        css.replaceValues(pattern, { fast: unit }, translate);
+      });
+    });
+  };
 });
--- test/test.js
+++ test/test.js
@@ -4,21 +4,26 @@ var expect  = require('chai').expect;
 var plugin = require('../');
 
 var test = function (input, output, opts, done) {
-    postcss([ plugin(opts) ]).process(input).then(function (result) {
-        expect(result.css).to.eql(output);
-        expect(result.warnings()).to.be.empty;
-        done();
-    }).catch(function (error) {
-        done(error);
-    });
+  postcss([ plugin(opts) ]).process(input).then(function (result) {
+    expect(result.css).to.eql(output);
+    expect(result.warnings()).to.be.empty;
+    done();
+  }).catch(function (error) {
+    done(error);
+  });
 };
 
 describe('postcss-imperial', function () {
-
-    /* Write tests here
-
-    it('does something', function (done) {
-        test('a{ }', 'a{ }', { }, done);
-    });*/
-
+  it('supports fathoms', function (done) {
+    test(
+      'a{ height: 1fathom; width: 2fathoms; perspective: 1.5ftm }',
+      'a{ height: 72in; width: 144in; perspective: 108in }',
+      {}, done);
+  });
+  it('supports twips', function (done) {
+    test(
+      'a{ letter-spacing: 1twip; font-size: 240twips }',
+      'a{ letter-spacing: 0.05pt; font-size: 12pt }',
+      {}, done);
+  });
 });
